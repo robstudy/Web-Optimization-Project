5aaefc891e9a440216efe32dd05b7f2699dcaf99
Robert Garza <[robstudent1988@gmail.com]>
2015-08-10 21:26:29 -0700

    replaced querySelector in resize Pizzas with variable

--- views/js/main.js
+++ views/js/main.js
@@ -401,18 +401,18 @@ var pizzaElementGenerator = function(i) {
 // resizePizzas(size) is called when the slider in the "Our Pizzas" section of the website moves.
 var resizePizzas = function(size) { 
   window.performance.mark("mark_start_resize");   // User Timing API function
-
+  var pizzaSizeVar = document.querySelector("#pizzaSize");
   // Changes the value for the size of the pizza above the slider
   function changeSliderLabel(size) {
     switch(size) {
       case "1":
-        document.querySelector("#pizzaSize").innerHTML = "Small";
+        pizzaSizeVar.innerHTML = "Small";
         return;
       case "2":
-        document.querySelector("#pizzaSize").innerHTML = "Medium";
+        pizzaSizeVar.innerHTML = "Medium";
         return;
       case "3":
-        document.querySelector("#pizzaSize").innerHTML = "Large";
+        pizzaSizeVar.innerHTML  = "Large";
         return;
       default:
         console.log("bug in changeSliderLabel");
